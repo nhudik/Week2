eda352cf2c738eda4d055d28b0f74c81c9c09bfb
nhudik <90761665+nhudik@users.noreply.github.com>
2023-01-19 13:52:05 +0200

    Update program.cs

--- program.cs
+++ program.cs
@@ -18,3 +18,16 @@ else
 {
     Console.WriteLine($"Welcome, {userLastName}!");
 }
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 - stack
+
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram");
+}
+else //if (userAge < 13)
+{
+    Console.WriteLine("You are too young to use Instagram");
+}
