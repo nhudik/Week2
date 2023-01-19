8b813cf8c7591a0d0a6fb526cc0e1f0b57b2a552
nhudik <90761665+nhudik@users.noreply.github.com>
2023-01-19 13:51:16 +0200

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,20 @@
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please, enter your last name");
+
+string userLastName = Console.ReadLine();
+
+
+if (userGender == 'm' )
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName} !");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
